c31b97c019a1ec74c76bd132077126af46e9e8d6
NandoVlr <cleversonando@gmail.com>
2025-11-01 12:46:03 -0300

    teste2

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,9 @@
+import express from 'express';
+import cors from 'cors';
 import app from '../app.js';
 
-const PORT = process.env.PORT || 3000;
+// Habilita CORS para todos os domínios
+app.use(cors());
+
+const PORT = process.env.PORT || 8080; // Railway usa 8080
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
